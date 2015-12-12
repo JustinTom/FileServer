fed1385ba3e1db3fcd556a705e51ee887cbdd1c6
JustinTom <cyclostofu@hotmail.com>
2015-12-12 10:52:12 -0800

    Initial commit of an older, previous project. All completed using Java language through Netbeans IDE

---
+++ FileSend/src/filesend/NetworkEmulator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesend;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author JT
+ */
+public class NetworkEmulator {
+    //Port numbers which will be open for the transmitter and receiver to connect with
+    public static int port = 7005;
+    public static int port2 = 7006;
+    
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
